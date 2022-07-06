f165806f79c76d88f68e8967d9b852b55892ed16
jonikulov <javohir200211@gmail.com>
2022-07-06 16:01:17 +0500

    modified chapter-6 py

--- auto-with-py.py
+++ auto-with-py.py
@@ -1,48 +1,6 @@
-## PAGE -> 179
-# Adding Bullets to Wiki Markup
-"""
+#### PAGE -> 192
+# Finding Patterns of Text Without Regular Expressions
 
-'''
-Lists of animals
-Lists of aquarium life
-Lists of biologists by author abbreviation
-Lists of cultivars
-'''
-
-import pyperclip
-text = pyperclip.paste()
-
-lines = text.split('\n')
-for i in range(len(lines)):
-    lines[i] = '* ' + lines[i]
-
-text = '\n'.join(lines)
-pyperclip.copy(text)
-"""
-######################################################################
-# Table Printer
-"""
-tableData = [['apples', 'oranges', 'cherries', 'banana'], \
-             ['Alice', 'Bob', 'Carol', 'David'], \
-             ['dogs', 'cats', 'moose', 'goose']]
-
-def printTable(Data):
-    # Get the maximum width of each column
-    colWidth = [0]*len(Data)
-    for i in range(len(Data)):
-        colWidth[i] = len(max(Data[i],key = len))
-        
-    # Print    
-    for j in range(len(Data[0])):
-        for i in range(len(Data)):
-            print(Data[i][j].rjust(colWidth[i]), end = ' ')
-        print('\r')
-
-printTable(tableData)
-""" 
-######################################################################
-# Finding phone numbers in a text
-"""
 message = "Call me at 415-555-1011 tomorrow. 645-012-9999 is my office."
 
 def isPhoneNumber(text):
@@ -59,84 +17,20 @@ for i in range(len(message)):
         print(f"Phone number: {chunk}")
 
 
+### PAGE 194
+# Finding Patterns of Text with Regular Expressions
 import re
-
 text = "345-965-3486"
 
 # phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
 mo = phoneNumRegex.search('My number is 415-555-4242.')
 print('Phone number found: ' + mo.group())
-"""
-######################################################################
-# PRACTICE PYTHON > Password Generator > Ex 16
-'''
-import random
 
-symbols = "!@#$%^&*=;:'~{}[]<>?_/*-+"
-up_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-low_letters = "abcdefghijklmnopqrstuvwxyz"
-
-def StrongPasswordGenerator():
-    password = ""
-    """16 character long, strong password generator"""
-    while True:
-        num = str(random.randint(1000, 9999))
-        if num[0]==num[1]==num[2] or num[1]==num[2]==num[3]:
-            continue
-        break
-    password += num
-
-    while True:
-        symbol = random.choice(symbols)
-        up_letter = random.choice(up_letters)
-        low_letter = random.choice(low_letters)
-
-        if (symbol not in password) and (up_letter not in password) and (low_letter not in password):
-            password += symbol
-            password += up_letter
-            password += low_letter
-
-        if len(password)==16:
-            break
-        continue
-
-
-    index_list = list(range(16))
-    pwd = ""
-    new_list = []
-
-    while True:
-        index = random.choice(index_list)
-        if index not in new_list:
-            new_list.append(index)
-            pwd += password[index]
-        if len(new_list)==16:
-            break
-        continue
-
-    return pwd
-
-print(StrongPasswordGenerator())
-'''
-######################################################################
-# password genereator
-"""
-import random, string
-
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-passlen = 8
-pwd =  "".join(random.sample(s,8))
-print(pwd)
-
-# print(string.digits)
-# print(string.ascii_letters)
-# print(string.punctuation)
-"""
 ######################################################################
 # Use the BeautifulSoup and requests Python packages to print out a
 # list of all the article titles on the New York Times homepage.
-"""
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -165,11 +59,13 @@ for story_heading in soup.find_all('h1'):
 #     for link in soup.find_all('h3'):
 #         file.write(link.get_text())
 #         file.write('\n')
-"""
+
 ######################################################################
-"""
-# mapIt.py - Launches a map in the browser using an
+
+# map_it.py - Launches a map in the browser using an
 # address from the command line or clipboard.
+# sample address: 870 Valencia St, San Francisco, CA 94110
+
 import webbrowser as wb
 import sys, pyperclip
 
@@ -187,13 +83,12 @@ for char in address:
 
 string_address = '+'.join(string_address.split())
 url = f"https://google.com/maps/place/{string_address}/"
-# wb.open(url)
+wb.open(url)
 print(url)
-# sample address: 870 Valencia St, San Francisco, CA 94110
-"""
+
 ######################################################################
 # BeautifulSoup > select method
-"""
+'''
 soup.select('div')                  - All elements named <div>
 soup.select('#author')              - The element with an id attribute of author
 soup.select('.notice')              - All elements that use a CSS class attribute named notice
@@ -202,4 +97,4 @@ soup.select('div > span')           - All elements named <span> that are directl
                                       named <div>, with no other element in between
 soup.select('input[name]')          - All elements named <input> that have a name attribute with any value
 soup.select('input[type="button"]') - All elements named <input> that have an attribute named type with value button
-"""
+'''
